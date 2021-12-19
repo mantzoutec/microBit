60e190ca94f82175ce9b56669f6e123229b83048
mantzoutec <mantzoutec@gmail.com>
2021-12-19 14:04:07 +0200

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 let ι = 0
+let λ = 0
 input.onButtonPressed(Button.A, function () {
     ι = 1
+    λ = 2
     while (ι <= 10) {
-        basic.showNumber(ι)
+        basic.showString("" + (ι))
+        basic.showString("*")
+        basic.showString("" + (λ))
+        basic.showString("=")
+        basic.showString("" + (ι * λ))
+        basic.showString("")
+        basic.showString("")
         // Τι θα συμβεί εαν ξεχάσω να χρησιμοποιησω την συγκεκριμένη εντολή;
         ι += 1
     }
