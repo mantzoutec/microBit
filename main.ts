1f8595c2189c56389d26786e53b65c9f249abdaf
mantzoutec <mantzoutec@gmail.com>
2021-12-19 13:40:43 +0200

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
-let i = 0
+let ι = 0
+let λ = 0
 input.onButtonPressed(Button.A, function () {
-    i = 0
-    while (i <= 50) {
-        basic.showNumber(i)
-        i += 1
+    ι = 1
+    while (ι <= 10) {
+        basic.showNumber(ι)
+        ι += 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    λ = 9
+    for (let ι = 0; ι <= 9; ι++) {
+        basic.showNumber(λ)
+        λ += -1
     }
 })
