f38e2afc7ffc78a0760564f216ea7bea258f523a
mantzoutec <mantzoutec@gmail.com>
2021-12-19 13:47:40 +0200

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,9 @@
 let ι = 0
-let λ = 0
 input.onButtonPressed(Button.A, function () {
     ι = 1
     while (ι <= 10) {
         basic.showNumber(ι)
+        // Τι θα συμβεί εαν ξεχάσω να χρησιμοποιησω την συγκεκριμένη εντολή;
         ι += 1
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    λ = 9
-    for (let ι = 0; ι <= 9; ι++) {
-        basic.showNumber(λ)
-        λ += -1
     }
 })
