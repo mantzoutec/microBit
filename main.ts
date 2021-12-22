1d59e1c190da0f2266322750556aab15c41a16a6
mantzoutec <mantzoutec@gmail.com>
2021-12-22 19:54:01 +0200

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let ι = 0
 let λ = 0
 input.onButtonPressed(Button.A, function () {
     ι = 1
-    λ = 2
+    λ = 5
     while (ι <= 10) {
         basic.showString("" + (ι))
         basic.showString("*")
@@ -13,5 +13,17 @@ input.onButtonPressed(Button.A, function () {
         basic.showString("")
         // Τι θα συμβεί εαν ξεχάσω να χρησιμοποιησω την συγκεκριμένη εντολή;
         ι += 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let i = 0; i <= 9; i++) {
+        for (let j = 0; j <= 9; j++) {
+            basic.showString("" + (i + 1))
+            basic.showString("*")
+            basic.showString("" + (j + 1))
+            basic.showString("=")
+            basic.showString("" + ((i + 1) * (j + 1)))
+            basic.showString("")
+        }
     }
 })
